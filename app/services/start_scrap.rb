ff39bd14d5fd305756740b7c8fb5d79505c183c5
Dyf-Tfh <n.zhao@hotmail.fr>
2018-08-09 11:13:04 +0200

    truc

--- app/services/start_scrap.rb
+++ app/services/start_scrap.rb
@@ -1,39 +1,41 @@
 class StartScrap
-
 @prices = []
 @cryptonames = []
 @hash = {}
 
-  def initialize(url)
-    @url = url
+  def initialize
   end
 
-  def perform
-    def get_crytocurrencies_names
-      webpage = Nokogiri::HTML(open(@url))
-      webpage.xpath('//td/a[@class = "currency-name-container link-secondary"]').each do |bli|
-        @cryptonames << bli.text
-      end
+  def get_crytocurrencies_names
+    webpage = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+    webpage.xpath('//td/a[@class = "currency-name-container link-secondary"]').each do |bli|
+      @cryptonames << bli.text
     end
+  end
 
-    def get_crytocurrencies_prices
-      page = Nokogiri::HTML(open(@url))
-      page.xpath('//td/a[@class = "price"]').each do |bam|
-        @prices << bam.text
-      end
+  def get_crytocurrencies_prices
+    page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
+    page.xpath('//td/a[@class = "price"]').each do |bam|
+      @prices << bam.text
     end
+  end
 
-    def hashfilling
-      s = 0
-      while s < @cryptonames.length
-        @hash[@cryptonames[s]] = @prices[s]
-        save
-        s = s + 1
-      end
+  def hashfilling
+    s = 0
+    while s < @cryptonames.length
+      @hash[@cryptonames[s]] = @prices[s]
+      save
+      s = s + 1
     end
   end
 
   def save
     Crypto.create(name: @cryptonames[s], value: @prices[s])
+  end
+
+  def perform
+    get_crytocurrencies_names
+    get_crytocurrencies_prices
+    hashfilling
   end
 end
