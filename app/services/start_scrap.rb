803742ef5ed976caa04fffc051463431e761634d
Dyf-Tfh <n.zhao@hotmail.fr>
2018-08-09 11:57:39 +0200

    form tag

--- app/services/start_scrap.rb
+++ app/services/start_scrap.rb
@@ -1,12 +1,15 @@
 class StartScrap
-@prices = []
-@cryptonames = []
-@hash = {}
+require 'nokogiri'
+require 'open-uri'
 
   def initialize
+    @cryptonames = []
+    @prices = []
+    @hash = {}
   end
 
   def get_crytocurrencies_names
+
     webpage = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
     webpage.xpath('//td/a[@class = "currency-name-container link-secondary"]').each do |bli|
       @cryptonames << bli.text
@@ -21,16 +24,16 @@ class StartScrap
   end
 
   def hashfilling
-    s = 0
-    while s < @cryptonames.length
-      @hash[@cryptonames[s]] = @prices[s]
+    @s = 0
+    while @s < @cryptonames.length
+      @hash[@cryptonames[@s]] = @prices[@s]
       save
-      s = s + 1
+      @s = @s + 1
     end
   end
 
   def save
-    Crypto.create(name: @cryptonames[s], value: @prices[s])
+    Crypto.create(name: @cryptonames[@s], value: @prices[@s])
   end
 
   def perform
