7ed4b5f158d545fc42c1fe1f40d3d9d8d975e8a8
nicolasculerier <nicolas.culerier@laposte.net>
2018-08-09 10:45:21 +0200

    Création service scrap

---
+++ app/services/start_scrap.rb
@@ -0,0 +1,38 @@
+class StartScrap
+
+@prices = []
+@cryptonames = []
+@hash = {}
+
+  def initialize(url)
+    @url = url
+  end
+
+  def perform
+    def get_crytocurrencies_names
+      webpage = Nokogiri::HTML(open(@url))
+      webpage.xpath('//td/a[@class = "currency-name-container link-secondary"]').each do |bli|
+        @cryptonames << bli.text
+      end
+    end
+
+    def get_crytocurrencies_prices
+      page = Nokogiri::HTML(open(@url))
+      page.xpath('//td/a[@class = "price"]').each do |bam|
+        @prices << bam.text
+      end
+    end
+
+    def hashfilling
+      s = 0
+      while s < @cryptonames.length
+        @hash[@cryptonames[s]] = @prices[s]
+        s = s + 1
+      end
+    end
+  end
+
+  def save
+
+  end
+end
